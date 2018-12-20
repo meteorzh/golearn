8f72db91b0a3284e2ad56cf5f1e99577b15bdef9
wenzhencn <wenzhencn@gmail.com>
2018-12-21 00:27:49 +0800

    完成求值表达式的内容

--- ch7/eval/eval.go
+++ ch7/eval/eval.go
@@ -3,12 +3,15 @@ package eval
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 // Expr: 算术表达式
 type Expr interface {
 	// Eval 返回表达式在 env 上下文下的值
 	Eval(env Env) float64
+	// Check 方法报告表达式中的错误，并把表达式中的变量加入 Vars 中
+	Check(vars map[Var]bool) error
 }
 
 // Var表示一个变量, 比如x
@@ -18,11 +21,20 @@ func (v Var) Eval(env Env) float64 {
 	return env[v]
 }
 
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
 // literal是一个数字常量, 比如3.141
 type literal float64
 
 func (l literal) Eval(env Env) float64 {
 	return float64(l)
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
 }
 
 // unary 表示一元操作符表达式
@@ -39,6 +51,13 @@ func (u unary) Eval(env Env) float64 {
 		return -u.x.Eval(env)
 	}
 	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
 }
 
 // binary 表示二元操作符表达式, 比如x+y
@@ -61,6 +80,16 @@ func (b binary) Eval(env Env) float64 {
 	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
 }
 
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpected binary op %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
 // call 表示函数调用表达式
 type call struct {
 	fn   string // one of "pow", "sin", "sqrt"中的一个
@@ -78,5 +107,23 @@ func (c call) Eval(env Env) float64 {
 	}
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
 
 type Env map[Var]float64
